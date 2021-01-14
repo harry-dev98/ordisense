754150335635282c3eddcfaa28cd2024a88af864
harry-dev98 <harjeet.769644@gmail.com>
2021-01-14 15:18:43 +0530

    api exports from one file

---
+++ src/service/api.js
@@ -0,0 +1,23 @@
+import {
+    fetchNotes,
+    addNote,
+    delNote
+} from './notes';
+
+import {
+    fetchData,
+} from './data';
+
+import {
+    verifyLogin,
+    verifyCaptcha
+} from './login';
+
+export {
+    fetchData,
+    fetchNotes,
+    addNote,
+    delNote,
+    verifyLogin,
+    verifyCaptcha,
+}
