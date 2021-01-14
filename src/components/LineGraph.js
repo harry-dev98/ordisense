b1935e53cde764e86df0c493e64293a6020307a5
harry-dev98 <harjeet.769644@gmail.com>
2021-01-14 16:37:59 +0530

    linegraph

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,35 +1,21 @@
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
 
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Temperature",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
 
 export default class LineDemo extends Component {
   render() {
+    const data = {
+      labels: this.props.labels,
+      datasets: [
+        {
+          label: this.props.label,
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          data: this.props.data
+        }
+      ]
+    };
     return <Line ref="chart" data={data} />;
   }
 
